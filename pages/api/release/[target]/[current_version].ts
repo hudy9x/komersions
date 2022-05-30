1eba12a24efb119d1ae118e568b84a5959c64c5e
hudy9x <hudy9x@gmail.com>
2022-05-31 00:20:31 +0700

    fix: return signature from .sig file, not from pubkey

--- pages/api/release/[target]/[current_version].ts
+++ pages/api/release/[target]/[current_version].ts
@@ -1,4 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from "axios";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { octokit } from "../../../../lib/git";
 
@@ -21,35 +22,54 @@ export default async function handler(
   res: NextApiResponse<unknown>
   // res: NextApiResponse<Version>
 ) {
-  const params = req.query;
+  try {
+    const params = req.query;
 
-  const { data } = await octokit.request("GET /repos/{owner}/{repo}/releases", {
-    owner: process.env.GITHUB_OWNER || "",
-    repo: process.env.GITHUB_REPO || "",
-  });
+    const { data } = await octokit.request(
+      "GET /repos/{owner}/{repo}/releases",
+      {
+        owner: process.env.GITHUB_OWNER || "",
+        repo: process.env.GITHUB_REPO || "",
+      }
+    );
 
-  const { tag_name, published_at, assets } = data[0];
-  const platforms: Platform = { windows: {}, macos: {}, linux: {} };
+    const { tag_name, published_at, assets } = data[0];
+    const platforms: Platform = { windows: {}, macos: {}, linux: {} };
 
-  assets.reduce((platforms, item) => {
-    const { browser_download_url, size, download_count, name } = item;
-    if (browser_download_url.indexOf(".msi")) {
-      platforms.windows = {
-        url: browser_download_url,
-        version: tag_name,
-        notes: "",
-        pub_date: published_at || "",
-        signature: process.env.SIGNATURE || "",
-      };
+    for (const item of assets) {
+      const { browser_download_url, size, download_count, name } = item;
+      if (/msi\.zip$/.test(browser_download_url)) {
+        platforms.windows = {
+          url: browser_download_url,
+          version: tag_name,
+          notes: "",
+          pub_date: published_at || "",
+          signature: "",
+        };
+      }
+
+      if (/msi\.zip\.sig$/.test(browser_download_url)) {
+        const { data: signature } = await axios(browser_download_url);
+        platforms.windows = {
+          ...platforms.windows,
+          ...{
+            signature,
+          },
+        };
+      }
     }
 
-    return platforms;
-  }, platforms);
+    // console.log(platforms);
 
-  if (params.target === "windows") {
-    res.status(200).json(platforms.windows);
-    return;
+    // res.status(200).json(platforms);
+
+    if (params.target === "windows") {
+      res.status(200).json(platforms.windows);
+      return;
+    }
+
+    res.status(204).send("No Content");
+  } catch (error) {
+    res.status(204).send(error);
   }
-
-  res.status(204).send("No Content");
 }
