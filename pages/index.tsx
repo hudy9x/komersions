8e1ad09a6f9a67d776ea22212634d2d64932ff98
hudy9x <hudy9x@gmail.com>
2022-05-31 11:05:21 +0700

    refactor: using getServerSideProps to render page on server side

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,92 +1,87 @@
 import axios from "axios";
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
 import dayjs from "dayjs";
 import { LatestVersion } from "../types";
-import { useEffect, useState } from "react";
 
-const Home: NextPage = () => {
-  const [loading, setLoading] = useState(true);
-  const [data, setData] = useState<LatestVersion>({
-    all_releases: "",
-    release_notes: "",
-    tag_name: "",
-    author: "",
-    published_at: "",
-    releases: [],
-  });
-
-  const {
-    all_releases,
-    release_notes,
-    tag_name,
-    author,
-    published_at,
-    releases,
-  } = data;
-
-  useEffect(() => {
-    axios(`/api/release/latest`)
-      .then(({ data: latestVersion }) => {
-        setData(latestVersion);
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  }, []);
-
+const Home: NextPage<LatestVersion> = ({
+  all_releases,
+  release_notes,
+  tag_name,
+  author,
+  published_at,
+  releases,
+}) => {
   const published = dayjs(published_at).fromNow();
 
   return (
     <div className="app">
       <div className="container">
-        {loading ? (
-          <h2 style={{ textAlign: "center" }}>Loading . . .</h2>
-        ) : (
-          <>
-            <div className="flex item-center justify-between">
-              <h1>{author}/{process.env.NEXT_PUBLIC_APP_NAME}</h1>
-              <span className="text-gray">{published}</span>
-            </div>
-            <ul>
-              {releases.map((release) => {
-                const s = parseFloat(release.size / 1024 / 1024 + "");
-                const size = s.toFixed(2);
+        <>
+          <div className="flex item-center justify-between">
+            <h2>
+              {author}/{process.env.NEXT_PUBLIC_APP_NAME}
+            </h2>
+            <span className="text-gray">{published}</span>
+          </div>
+          <ul>
+            {releases.map((release) => {
+              const s = parseFloat(release.size / 1024 / 1024 + "");
+              const size = s.toFixed(2);
+              const url = release.browser_download_url;
+              const isWin = url.indexOf(".msi");
 
-                const isWin = release.browser_download_url.indexOf(".msi");
+              if (!/msi\.zip$/.test(url)) {
+                return null;
+              }
 
-                return (
-                  <li
-                    key={release.name}
-                    className="flex item-center justify-between"
-                  >
-                    <a href={release.browser_download_url}>
-                      {/* <span>{isWin ? "win86x64" : ""}</span> */}
-                      {release.name}
-                    </a>
-                    <span className="text-gray-2">{size} mb</span>
-                  </li>
-                );
-              })}
-            </ul>
-            <div className="flex item-center justify-between">
-              <div className="flex item-center gap-2">
-                <h4 className="version button-70">{tag_name}</h4>
-                <a className="text-gray-3" href={release_notes}>
-                  Release Note
-                </a>
-              </div>
-              <a className="text-gray-3" href={all_releases}>
-                All Releases
+              return (
+                <li
+                  key={release.name}
+                  className="flex item-center justify-between"
+                >
+                  <a href={release.browser_download_url}>
+                    <span style={{ color: "white", paddingRight: 10 }}>
+                      {isWin ? "win86x64" : ""}
+                    </span>
+                    {release.name}
+                  </a>
+                  <span className="text-gray-2">{size} mb</span>
+                </li>
+              );
+            })}
+          </ul>
+          <div className="flex item-center justify-between">
+            <div className="flex item-center gap-2">
+              <h4 className="version button-70">{tag_name}</h4>
+              <a className="text-gray-3" href={release_notes}>
+                Release Note
               </a>
             </div>
-          </>
-        )}
+            <a className="text-gray-3" href={all_releases}>
+              All Releases
+            </a>
+          </div>
+        </>
       </div>
     </div>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async function ({ res }) {
+  try {
+    res.setHeader(
+      "Cache-Control",
+      "public, s-maxage=10, stale-while-revalidate=59"
+    );
+
+    const { data: latestVersion } = await axios(
+      `${process.env.URL}/api/release/latest`
+    );
+    return { props: latestVersion };
+  } catch (error) {
+    console.log(error);
+    return { props: {} };
+  }
 };
 
 export default Home;
