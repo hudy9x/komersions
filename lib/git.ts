3a32a21ce304217ef631b8d87d9116215900e019
hudy9x <95471659+hudy9x@users.noreply.github.com>
2023-04-18 11:37:08 +0700

    Update git.ts

--- lib/git.ts
+++ lib/git.ts
@@ -1,5 +1,7 @@
 import { Octokit } from "octokit";
 
 export const octokit = new Octokit({
+  // Generate Private Key refers to: https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token
+  // Note that: current key is Tokens (classic) not Fine-grained tokens
   auth: process.env.PRIVATE_KEY,
 });
